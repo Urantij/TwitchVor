62d30de2777201c624f3401b5677da03f8b33398
Urantij <maximumuranti@gmail.com>
2023-03-02 18:35:43 +0700

    Добавит логерфактори в базовый чекер

--- TwitchVor/Twitch/Checker/BaseChecker.cs
+++ TwitchVor/Twitch/Checker/BaseChecker.cs
@@ -8,12 +8,14 @@ namespace TwitchVor.Twitch.Checker;
 
 public abstract class BaseChecker
 {
-    protected ILogger _logger;
+    protected readonly ILoggerFactory _loggerFactory;
+    protected readonly ILogger _logger;
 
     public event EventHandler<TwitchCheckInfo>? ChannelChecked;
 
     protected BaseChecker(ILoggerFactory loggerFactory)
     {
+        this._loggerFactory = loggerFactory;
         _logger = loggerFactory.CreateLogger(this.GetType());
     }
 
