2c390d8806771f74f86228a85c27dbe780fbcf47
Urantij <maximumuranti@gmail.com>
2023-03-02 19:16:08 +0700

    Обновит зависимости, заменит пабсаб на МОЙ пабсаб

--- TwitchVor/Twitch/Checker/PubsubChecker.cs
+++ TwitchVor/Twitch/Checker/PubsubChecker.cs
@@ -1,132 +1,76 @@
 using Microsoft.Extensions.Logging;
-using TwitchLib.PubSub;
-using TwitchLib.PubSub.Events;
+using TwitchSimpleLib.Pubsub;
+using TwitchSimpleLib.Pubsub.Payloads.Playback;
 using TwitchVor.Utility;
 
 namespace TwitchVor.Twitch.Checker
 {
     class PubsubChecker : BaseChecker
     {
-        private TwitchPubSub? client;
+        private readonly TwitchPubsubClient client;
 
         public DateTime? debug_LastStreamEvent = null;
 
         public PubsubChecker(ILoggerFactory loggerFactory)
             : base(loggerFactory)
         {
+            client = new TwitchPubsubClient(new TwitchPubsubClientOpts(), loggerFactory);
+            if (Program.config.ChannelId is null)
+                throw new NullReferenceException(nameof(Program.config.ChannelId));
+
+            client.AddPlaybackTopic(Program.config.ChannelId);
+
+            client.Connected += Connected;
+            client.ConnectionClosed += ConnectionClosed;
+            client.PlaybackReceived += PlaybackReceived;
+        }
+
+        private void Connected()
+        {
+            _logger.LogInformation("Connected.");
+        }
+
+        private void ConnectionClosed(Exception? ex)
+        {
+            _logger.LogWarning(ex, "ConnectionClosed.");
+        }
+
+        private void PlaybackReceived((string channelId, PlaybackData) args)
+        {
+            PlaybackData data = args.Item2;
+
+            bool status;
+            if (data.Type == "stream-up")
+            {
+                status = true;
+            }
+            else if (data.Type == "stream-down")
+            {
+                status = false;
+            }
+            else
+            {
+                _logger.LogError("Непонятный тип в плейбеке {type}", data.Type);
+                return;
+            }
+
+            DateTime time = DateTimeOffset.FromUnixTimeSeconds(data.ServerTime).UtcDateTime;
+
+            TwitchCheckInfo checkInfo = new(status, time);
+            try
+            {
+                OnChannelChecked(checkInfo);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "PlaybackReceived");
+            }
         }
 
         public void Start()
         {
-            var thisClient = client = new TwitchPubSub();
-            try
-            {
-                thisClient.OnPubSubServiceConnected += PubSubServiceConnected;
-                thisClient.OnListenResponse += ListenResponse;
-                thisClient.OnPubSubServiceClosed += PubSubServiceClosed;
-                thisClient.OnPubSubServiceError += PubSubServiceError;
-                thisClient.OnStreamUp += StreamUp;
-                thisClient.OnStreamDown += StreamDown;
-
-                thisClient.ListenToVideoPlayback(Program.config.ChannelId);
-
-                _logger.LogInformation("Connecting...");
-                thisClient.Connect();
-            }
-            catch (Exception e)
-            {
-                thisClient.OnPubSubServiceConnected -= PubSubServiceConnected;
-                thisClient.OnListenResponse -= ListenResponse;
-                thisClient.OnPubSubServiceClosed -= PubSubServiceClosed;
-                thisClient.OnPubSubServiceError -= PubSubServiceError;
-                thisClient.OnStreamUp -= StreamUp;
-                thisClient.OnStreamDown -= StreamDown;
-
-                _logger.LogError(e, "Ошибка при подключении.");
-
-                PubSubServiceClosed(thisClient, EventArgs.Empty);
-            }
-        }
-
-        private void PubSubServiceConnected(object? sender, EventArgs e)
-        {
-            var senderClient = sender as TwitchPubSub;
-            if (senderClient != client || client == null)
-                return;
-
-            _logger.LogInformation("Connected. Sending topics.");
-            senderClient!.SendTopics(); //не может быть нул, сверху проверка
-        }
-
-        private void ListenResponse(object? sender, OnListenResponseArgs e)
-        {
-            if (e.Successful)
-            {
-                _logger.LogInformation("Listening ({Topic})", e.Topic);
-            }
-            else
-            {
-                _logger.LogError("Failed to listen! Response: ({Topic})", e.Topic);
-            }
-        }
-
-        private void PubSubServiceClosed(object? sender, EventArgs e)
-        {
-            var senderClient = sender as TwitchPubSub;
-            if (senderClient != client || client == null)
-                return;
-
-            _logger.LogInformation($"Closed.");
-            client = null;
-            senderClient!.Disconnect(); //не может быть нул. Сверху проверка
-
-            Task.Run(async () =>
-            {
-                await Task.Delay(Program.config.PubsubReconnectDelay);
-
-                //Такого не должно быть, но я ебал эту либу
-                if (client != null)
-                    return;
-
-                Start();
-            });
-        }
-
-        private void PubSubServiceError(object? sender, OnPubSubServiceErrorArgs e)
-        {
-            _logger.LogError(e.Exception, "PubSubServiceError");
-        }
-
-        private void StreamUp(object? sender, OnStreamUpArgs e)
-        {
-            debug_LastStreamEvent = DateTime.UtcNow;
-
-            var checkInfo = new TwitchCheckInfo(true, DateTime.UtcNow);
-
-            try
-            {
-                OnChannelChecked(checkInfo);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "onStreamUp");
-            }
-        }
-
-        private void StreamDown(object? sender, OnStreamDownArgs e)
-        {
-            debug_LastStreamEvent = DateTime.UtcNow;
-
-            var checkInfo = new TwitchCheckInfo(false, DateTime.UtcNow);
-
-            try
-            {
-                OnChannelChecked(checkInfo);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "onStreamDown");
-            }
+            _logger.LogInformation("Connecting...");
+            client.ConnectAsync();
         }
     }
 }
