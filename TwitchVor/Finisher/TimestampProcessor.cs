e4ccf30db42915ad17aaf8e454174936ef8a5cca
Urantij <maximumuranti@gmail.com>
2025-03-14 16:35:28 +0700

    Поправит добавление клипов в описание видео

--- TwitchVor/Finisher/TimestampProcessor.cs
+++ TwitchVor/Finisher/TimestampProcessor.cs
@@ -13,20 +13,23 @@ internal static class TimestampProcessor
     public static async Task DoClips(StreamHandler streamHandler, List<BaseTimestamp> timestamps, ILogger logger)
     {
         Clip[] clips = streamHandler.chatWorker.CloneFetchedClips();
-        if (clips.Length > 0)
+        if (clips.Length == 0)
+            return;
+
+        try
         {
+            clips = clips.Where(clip =>
+            {
+                DateTime date = DateTime.Parse(clip.CreatedAt);
+
+                return date > streamHandler.handlerCreationDate;
+            }).ToArray();
+
+            if (clips.Length == 0)
+                return;
+
             try
             {
-                clips = clips.Where(clip =>
-                {
-                    DateTime date = DateTime.Parse(clip.CreatedAt);
-
-                    return date > streamHandler.handlerCreationDate;
-                }).ToArray();
-                
-                if (clips.Length == 0)
-                    return;
-
                 GetClipsResponse clipsResponse =
                     await Program.twitchAPI.Helix.Clips.GetClipsAsync(
                         clipIds: clips.Select(c => c.Id).ToList());
@@ -42,78 +45,92 @@ internal static class TimestampProcessor
 
                     return updatedClip ?? clip;
                 }).ToArray();
+            }
+            catch (Exception e)
+            {
+                logger.LogWarning(e, "Не удалось обновить информацию о клипах.");
+            }
 
-                List<string> videoIds = clips.Select(clip => clip.VideoId)
-                    .Distinct()
-                    .Where(s => !string.IsNullOrEmpty(s))
-                    .ToList();
+            List<string> videoIds = clips.Select(clip => clip.VideoId)
+                .Distinct()
+                .Where(s => !string.IsNullOrEmpty(s))
+                .ToList();
 
-                if (videoIds.Count > 0)
+            Video[]? videos = null;
+            if (videoIds.Count > 0)
+            {
+                try
                 {
                     GetVideosResponse vids =
                         await Program.twitchAPI.Helix.Videos.GetVideosAsync(videoIds: videoIds);
 
-                    // Бредик
-                    DateTime EstimateClipLocation(Clip clip, DateTime created, ChatClipConfig config)
+                    videos = vids.Videos;
+                }
+                catch (Exception e)
+                {
+                    logger.LogWarning(e, "Не удалось получить ответ с видиками.");
+                }
+            }
+            else
+            {
+                logger.LogWarning("Нет видео для клипов.");
+            }
+
+            // Бредик
+            DateTime EstimateClipLocation(Clip clip, DateTime created, ChatClipConfig config)
+            {
+                return created - TimeSpan.FromSeconds(clip.Duration) + config.ClipOffset;
+            }
+
+            List<ChatClipTimestamp> clipStamps = new();
+            foreach (Clip clip in clips)
+            {
+                // этот код выглядит всрато, но я не буду его переписывать.
+                Video? video = videos?.FirstOrDefault(v => v.Id == clip.VideoId);
+
+                DateTime clipDate;
+                if (video != null)
+                {
+                    // Иногда в чат кидают новые клипы со старых водов. Удивительное дело.
+                    DateTime videoCreatedAt = DateTime.Parse(video.CreatedAt);
+
+                    // Небольшой оффсет, так как непонятно...
+                    if (videoCreatedAt + TimeSpan.FromMinutes(30) < streamHandler.handlerCreationDate)
                     {
-                        return created - TimeSpan.FromSeconds(clip.Duration) + config.ClipOffset;
+                        logger.LogInformation("Кто то закинул старый клип... хух {clip}", clip.Id);
+                        continue;
                     }
 
-                    List<ChatClipTimestamp> clipStamps = new();
-                    foreach (Clip clip in clips)
+                    if (clip.VodOffset != 0)
                     {
-                        // этот код выглядит всрато, но я не буду его переписывать.
-                        Video? video = vids.Videos.FirstOrDefault(v => v.Id == clip.VideoId);
-
-                        DateTime clipDate;
-                        if (video != null)
-                        {
-                            // Иногда в чат кидают новые клипы со старых водов. Удивительное дело.
-                            DateTime videoCreatedAt = DateTime.Parse(video.CreatedAt);
-
-                            // Небольшой оффсет, так как непонятно...
-                            if (videoCreatedAt + TimeSpan.FromMinutes(30) < streamHandler.handlerCreationDate)
-                            {
-                                logger.LogInformation("Кто то закинул старый клип... хух {clip}", clip.Id);
-                                continue;
-                            }
-
-                            if (clip.VodOffset != 0)
-                            {
-                                clipDate = videoCreatedAt + TimeSpan.FromSeconds(clip.VodOffset);
-
-                                clipStamps.Add(new ChatClipTimestamp(clip.CreatorName, clip.CreatorId,
-                                    clip.Title, clip.Url, clipDate));
-                                continue;
-                            }
-
-                            logger.LogWarning("Для клипа нет оффсета {clip}", clip.Id);
-                        }
-                        else
-                        {
-                            logger.LogWarning("Не удалось найти видео для клипа {clip} ({id})", clip.Id,
-                                clip.VideoId);
-                        }
-
-                        DateTime clipCreatedAt = DateTime.Parse(clip.CreatedAt);
-
-                        clipDate = EstimateClipLocation(clip, clipCreatedAt, Program.config.Chat?.FetchClips);
+                        clipDate = videoCreatedAt + TimeSpan.FromSeconds(clip.VodOffset);
 
                         clipStamps.Add(new ChatClipTimestamp(clip.CreatorName, clip.CreatorId,
                             clip.Title, clip.Url, clipDate));
+                        continue;
                     }
 
-                    timestamps.AddRange(clipStamps);
+                    logger.LogWarning("Для клипа нет оффсета {clip}", clip.Id);
                 }
                 else
                 {
-                    logger.LogWarning("Нет видео для клипов.");
+                    logger.LogWarning("Не удалось найти видео для клипа {clip} ({id})", clip.Id,
+                        clip.VideoId);
                 }
+
+                DateTime clipCreatedAt = DateTime.Parse(clip.CreatedAt);
+
+                clipDate = EstimateClipLocation(clip, clipCreatedAt, Program.config.Chat?.FetchClips);
+
+                clipStamps.Add(new ChatClipTimestamp(clip.CreatorName, clip.CreatorId,
+                    clip.Title, clip.Url, clipDate));
             }
-            catch (Exception e)
-            {
-                logger.LogError(e, "Не удалось проанализировать твич воды.");
-            }
+
+            timestamps.AddRange(clipStamps);
+        }
+        catch (Exception e)
+        {
+            logger.LogError(e, "Не удалось проанализировать твич воды.");
         }
     }
 }
